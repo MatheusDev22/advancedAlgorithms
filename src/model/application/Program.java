21c48901c982a541748ca552e69720c9b718a42c
MatheusDev22 <matheusbsb95@gmail.com>
2022-03-04 10:22:27 -0300

    using do while

--- src/model/application/Program.java
+++ src/model/application/Program.java
@@ -1,9 +1,39 @@
 package model.application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+
 public class Program {
 	
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		int qtd = 1;
+		int cont = 0;
+		double soma = 0;
+		double notas;
+		char op;
+		do {
+		
+				System.out.print("\nInforme a nota do " + qtd + "º Aluno: ");
+				notas = sc.nextDouble();
+				soma += notas;
+				qtd++;
+				cont++;
+				sc.nextLine();
+				
+			
+			System.out.print("\nDeseja cadastrar " + qtd + "ª Nota? (S/N): ");
+			 op = sc.next().charAt(0);
+			
+		}while( op == 's' || op == 'S');
+		
+		System.out.println(" Media Aritmetica = " + soma/cont);
+		
+		sc.close();
 	}
 
 }
